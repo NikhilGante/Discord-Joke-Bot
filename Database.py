23005bdf1d7d36bcc4865954691cb684194dd82f
NikhilGante <55672843+NikhilGante@users.noreply.github.com>
2021-04-15 01:00:00 -0400

    "Rank" function gives requested hits for a specific user

--- Database.py
+++ Database.py
@@ -6,12 +6,12 @@ import random
 max_len = 68 # max amount of lines that can be sent on discord
 
 class Database:
-    path = ""
-    fn = [] # fieldnames
+    path = ""   # path for .csv where database is contained
+    fn = [] # fieldnames (string names to index list of dictionaries)
     channel = ""
-    name = ""
-    final_string = ""
-    field_strings = []
+    name = ""   # name of database to use when printing
+    final_string = ""   # final string to print
+    field_strings = []  # strings that contain all the elements in a field (e.g. "User")
     embed_colour = 0x3a5af2
 
     def write_header(self):
@@ -47,6 +47,7 @@ class Database:
             await context.send(f"All {self.name.lower()}s successfully deleted.")
         else:   await context.send("Only supreme memer CodingBoy56 has access to this command.")
 
+    # Function for user to erase a specific index from database
     async def erase(self, context, index):
         index = int(index)
         self.download_data()
@@ -62,6 +63,7 @@ class Database:
             self.upload_data()
             await context.send(f"{self.name} {index} succesfully deleted.")           
 
+    # Function for user to print out contents of a database
     async def show(self, context):
         self.download_data()
         Embed = discord.Embed(title = self.name, description = f"All of your {self.name.lower()}s so far.", color = self.embed_colour)
@@ -72,13 +74,12 @@ class Database:
         for string in range(len(self.field_strings)):
             Embed.add_field(name = self.fn[string], value = self.field_strings[string], inline = True)
 
-            # self.fn[string]
-            # self.field_strings[string]
         # Embed.add_field(name = "Sample name: ", value = "sample value", inline = True)
         # Embed.set_footer(text = "footer shit haha")
         # Embed.set_author(name = "Nikki the chiken")
         await context.send(embed = Embed)
 
+    # Function for user to access specific index of a database
     async def get(self, context, index, user: discord.Member = None):
         self.download_data()
         index = int(index)
@@ -112,6 +113,7 @@ class Rankings(Database):
 
         self.upload_data()
     
+    # Function for user to print out the desired amount of (ordered) rankings
     async def get_rank(self, context, amount):
         self.sort_data()
         amount = int(amount)
@@ -142,11 +144,12 @@ class Rankings(Database):
         
         await context.send(embed = Embed)
 
+    # Function for user to rank another user, and select the amount of indices printed out
     async def rank(self, context, amount, user: discord.Member = None):
         self.sort_data()
         amount = int(amount)
-        final_msg = "**Ranking:\t\t\tLength:**" # adds header to message
 
+        # user_sorted_rankings is filled with a list of dictionaries pertaining to the requested user
         user_sorted_rankings = [row for row in self.data if row[self.fn[1]] == str(user)]
 
         if amount == 0: # if no user input, send all available rows for requested user in database
@@ -160,19 +163,19 @@ class Rankings(Database):
         elif amount > max_len:    # if discord can't send all rows, send as many as possible
             amount = max_len
         else:
-            print("Unknown condition")
-        for row in range(amount):    # add rows to message
-            final_msg += f"\n{user_sorted_rankings[row][self.fn[0]]}\t\t\t\t\t\t"\
-            f" {user_sorted_rankings[row][self.fn[2]]}"
+            print(f"Sucess or Unknown condition triggered | amount received: {amount}")
             
-        # show message
+        # shows message
         self.download_data()
         Embed = discord.Embed(title = f"{self.name}s", description = f"All of {user}'s {self.name.lower()}s so far.", color = self.embed_colour)
         self.field_strings = ["", "", ""]
         
+        # fills field_strings with contents of user_sorted_rankings
         for row in range(amount):
             for field in range(len(self.data[row])):
-                self.field_strings[field] += f"\n{self.data[row][self.fn[field]]}"
+                self.field_strings[field] += f"\n{user_sorted_rankings[row][self.fn[field]]}"
+        
+        # prints the 3 embeds inline
         for string in range(len(self.field_strings)):
             Embed.add_field(name = self.fn[string], value = self.field_strings[string], inline = True)
             # Embed.set_footer(text = "footer shit haha")
@@ -196,10 +199,10 @@ Jokes.name = "Joke"
 Quotes.name = "Quote"
 
 # defines csv file paths
-Insults.path = "C:/Users/HP/OneDrive/Documents/GitHub/Discord-Insult-Bot-Database/Insults.csv"
-PP_Length.path = "C:/Users/HP/OneDrive/Documents/GitHub/Discord-Insult-Bot-Database/PP_Length.csv"
-Jokes.path = "C:/Users/HP/OneDrive/Documents/GitHub/Discord-Insult-Bot-Database/Jokes.csv"
-Quotes.path = "C:/Users/HP/OneDrive/Documents/GitHub/Discord-Insult-Bot-Database/Quotes.csv"
+Insults.path = "C:/Users/HP/Documents/GitHub/Discord-Insult-Bot-Database/Insults.csv"
+PP_Length.path = "C:/Users/HP/Documents/GitHub/Discord-Insult-Bot-Database/PP_Length.csv"
+Jokes.path = "C:/Users/HP/Documents/GitHub/Discord-Insult-Bot-Database/Jokes.csv"
+Quotes.path = "C:/Users/HP/Documents/GitHub/Discord-Insult-Bot-Database/Quotes.csv"
 
 # defines channels
 Insults.channel = "bot-insults"
@@ -216,7 +219,7 @@ Quotes.fn = ["Index:", "Author:", "Quote:"]
 Insults.embed_colour = 0x3a5af2
 PP_Length.embed_colour = 0x1e8c34
 Jokes.embed_colour = 0x23eeb7
-Quotes.embed_colour = 0x06dba3
+Quotes.embed_colour = 0x160bd3
 
 def truncate(n, decimals = 0):
     multiplier = 10 ** decimals
--- Help_Commands.py
+++ Help_Commands.py
@@ -2,10 +2,10 @@ import discord
 from discord.ext import commands
 from Miscellaneous import client
 
+# client.remove_command("help")
 # client.group(invoke_without_command = True)
-client.remove_command("help")
-client.command(aliases = "h")
-async def help(context):
+@client.command(aliases = ["h"])
+async def help_me(context):
     Embed = discord.Embed(title = "Help", description = "Use help <command> for more detailed a description.", color = context.author.color)
 
     Embed.add_field(name = "Database", value = "erase_all_insults, erase_all_jokes, erase_all_quotes, erase_insult, erase_joke, erase_quote", inline = False)
